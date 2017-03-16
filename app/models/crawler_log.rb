2e055f32521d671c86210829ceb290f9a6e9fff8
Gabriel Lima <gabriellimatrajano@hotmail.com>
2017-03-15 21:32:32 -0300

    subindo as alters no logs

--- app/models/crawler_log.rb
+++ app/models/crawler_log.rb
@@ -8,10 +8,26 @@ class CrawlerLog < ActiveRecord::Base
 
   def add_message(message)
     self.message.concat("#{message}|")
+    case message
+    when contains("processado com sucesso!")
+      self.category_1.concat("#{message}|-------------------|")
+
+    when contains("Mais de 5")
+      self.category_2.concat("#{message}|-------------------|")
+
+    when contains("não disponível na aliexpress!")
+      self.category_3.concat("#{message}|-------------------|")
+
+    when contains("Link aliexpress não cadastrado")
+      self.category_4.concat("#{message}|-------------------|")
+
+    when contains("importar do wordpress")
+      self.category_5.concat("#{message}|-------------------|")
+    end
     self.save!
   end
 
-  def get_message
-    self.message.split("|")
-  end
+  # def get_message
+  #   self.message.split("|")
+  # end
 end
